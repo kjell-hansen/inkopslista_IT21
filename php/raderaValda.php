1073774fdbd0d4dd84a987f9e54d303fb633d6ca
Kjell Hansen (S1-KJHA-2905) <kjell.hansen@gymnasium.ax>
2023-05-17 14:30:55 +0300

    Radera valda

--- php/raderaValda.php
+++ php/raderaValda.php
@@ -1,2 +1,24 @@
 <?php
-declare (strict_types=1);
+declare(strict_types=1);
+
+// Lägg till gemensamma funktioner
+require_once "funktioner.php";
+
+// Kontrollera metod
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+    $error = new stdClass();
+    $error->meddelande = ["Wrong method", "Sidan ska anropas med POST"];
+    skickaJSON($error, 405);
+}
+
+// Koppla databas
+$db = connectDB();
+
+// Radera valda varor
+$sql = "DELETE FROM varor WHERE checked=1";
+$stmt = $db->query($sql);
+
+// Skicka svar
+$out = new stdClass();
+$out->meddelande = "OK";
+skickaJSON($out);
