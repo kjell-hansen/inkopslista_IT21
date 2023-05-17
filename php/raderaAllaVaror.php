ae34a876219bb9aedb546a382534669cdb27de31
Kjell Hansen (S1-KJHA-2905) <kjell.hansen@gymnasium.ax>
2023-05-17 14:43:12 +0300

    Radera alla varor och test i Postman fungerar

--- php/raderaAllaVaror.php
+++ php/raderaAllaVaror.php
@@ -1,2 +1,22 @@
 <?php
-declare (strict_types=1);
+declare(strict_types=1);
+
+// Lägg till gemensamma funktioner
+require_once "funktioner.php";
+
+// Kontrollera metod
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+    $error = new stdClass();
+    $error->meddelande = ["Wrong method", "Sidan ska anropas med POST"];
+    skickaJSON($error, 405);
+}
+
+// Koppla databas
+$db = connectDB();
+
+// Radera alla varor
+$sql = "DELETE FROM varor";
+$db->query($sql);
+
+// Skicka svar
+skickaJSON(["meddelande" => "OK"]);
