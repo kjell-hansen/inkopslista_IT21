3b44391e8ff75e334ee8ca85ddd2e681b9367150
Kjell Hansen (S1-KJHA-2905) <kjell.hansen@gymnasium.ax>
2023-05-22 13:17:43 +0300

    Lagt till ram i alla testfiler

---
+++ test/testUppdateraVara.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+
+try {
+
+} catch (Exception $e) {
+    echo "<p class='error'>";
+    echo "Något gick JÄTTEfel!<br>";
+    echo $e->getMessage();
+    echo "</p>";
+}
+
+exit;
