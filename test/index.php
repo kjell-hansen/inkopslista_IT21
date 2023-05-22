8a01c01813de1c96bc3365545632e93a7e4c4e51
Kjell Hansen (S1-KJHA-2905) <kjell.hansen@gymnasium.ax>
2023-05-22 12:59:21 +0300

    Lagt till filer för automatisk testning

---
+++ test/index.php
@@ -0,0 +1,34 @@
+<?php
+
+
+
+
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Testsida för inköpslistan</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="test.css">
+</head>
+
+<body>
+    <h1>Testsida för API-anrop</h1>
+    <h2>Hämta alla varor</h2>
+    <?php require_once "testHamtaAlla.php" ?>
+    <h2>Kryssa vara</h2>
+    <?php require_once "testKryssaVara.php" ?>
+    <h2>Radera alla varor</h2>
+    <?php require_once "testRaderaAllaVaror.php" ?>
+    <h2>Radera valda varor</h2>
+    <?php require_once "testRaderaValda.php" ?>
+    <h2>Radera enskild vara</h2>
+    <?php require_once "testRaderaVara.php" ?>
+    <h2>Spara vara</h2>
+    <?php require_once "testSparaVara.php" ?>
+    <h2>Uppdatera vara</h2>
+    <?php require_once "testUppdateraVara.php" ?>
+</body>
+
+</html>
