eca667c183cb5c52b9497f70292badca2d5d4f24
Kjell Hansen (S1-KJHA-2905) <kjell.hansen@gymnasium.ax>
2023-05-25 12:25:49 +0300

    Test radera enskild vara klart

--- test/testRaderaValda.php
+++ test/testRaderaValda.php
@@ -1,11 +1,54 @@
 <?php
 declare(strict_types=1);
+require_once "../php/funktioner.php";
 
 try {
+    // Skapa handle till cUrl för att läsa svaret
+    $ch = curl_init('http://localhost/inkopslista/php/raderaValda.php');
+
+    // Se till att vi får svaret som en sträng
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+    // Anropen till sidan som ska testas
+    // Fel metod
+    echo "<p class='info'>Test fel metod</p>";
+    felMetod($ch);
+
+    // Test ok
+    echo "<p class='info'>Test radera valda OK</p>";
+    raderaValda($ch);
 
 } catch (Exception $e) {
     echo "<p class='error'>";
     echo "Något gick JÄTTEfel!<br>";
     echo $e->getMessage();
     echo "</p>";
+}
+
+function raderaValda($curlHandle)
+{
+    // Koppla databas
+    $db = connectDB();
+
+    // Läs in alla varor (för att återställa senare)
+    $varor = hamtaAllaVaror();
+
+    // Kryssa alla varor
+    foreach ($varor as $value) {
+        kryssaVara($value['id']);
+    }
+
+    // Anropa sidan
+    curl_setopt($curlHandle, CURLOPT_POST, true);
+    $jsonSvar = curl_exec($curlHandle);
+    $status = curl_getinfo($curlHandle, CURLINFO_RESPONSE_CODE);
+
+    // Kontrollera svaret
+    if ($status === 200) {
+        echo "<p class='ok'>Radera valda varor fungerade</p>";
+    } else {
+        echo "<p class='error'>Radera valda varor fungerade inte, fick status=$status istället för 200</p>";
+    }
+    // Återställ alla varor
+    aterstallDB($varor);
 }
--- test/testRaderaVara.php
+++ test/testRaderaVara.php
@@ -1,11 +1,67 @@
 <?php
 declare(strict_types=1);
+require_once "../php/funktioner.php";
 
 try {
+    // Skapa handle till cUrl för att läsa svaret
+    $ch = curl_init('http://localhost/inkopslista/php/raderaVara.php');
+
+    // Se till att vi får svaret som en sträng
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+    // Anropen till sidan som ska testas
+    // Fel metod
+    echo "<p class='info'>Test fel metod</p>";
+    felMetod($ch);
+
+    // Anropa utan id
+    echo "<p class='info'>Testa anropa utan id</p>";
+    idSaknas($ch);
+
+    // Anropa med id som inte finns
+    echo "<p class='info'>Testa anropa med id som inte finns</p>";
+    idFinnsInte($ch);
+
+    // Anropa med ogiltigt id (-1)
+    echo "<p class='info'>Testa anropa med ogiltigt id (-1)</p>";
+    idNegativt($ch);
+
+    // Anropa med felaktigt id (bokstav)
+    echo "<p class='info'>Testa anropa med felaktigt id (bokstav)</p>";
+    idBokstav($ch);
+
+    // Anropa med id som finns - OK
+    echo "<p class='info'>Testa korrekt anrop</p>";
+    idOKRaderaVara($ch);
 
 } catch (Exception $e) {
     echo "<p class='error'>";
     echo "Något gick JÄTTEfel!<br>";
     echo $e->getMessage();
     echo "</p>";
+}
+
+function idOKRaderaVara($curlHandle)
+{
+    // Koppla databas
+    $db = connectDB();
+
+    // Skapa vara
+    $id = skapaVara('test');
+
+    // Anropa sidan
+    curl_setopt($curlHandle, CURLOPT_POST, true);
+    $data = ['id' => $id];
+    curl_setopt($curlHandle, CURLOPT_POSTFIELDS, $data);
+    $jsonSvar = curl_exec($curlHandle);
+    $status = curl_getinfo($curlHandle, CURLINFO_RESPONSE_CODE);
+
+    // Kontrollera svaret
+    if ($status === 200) {
+        echo "<p class='ok'>Radera vara fungerade som förväntat</p>";
+    } else {
+        echo "<p class='error'>Radera vara returnerade status=$status istället för förväntat 200</p>";
+        raderaVara($id);
+    }
+
 }
